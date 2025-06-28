4406e3723f7de1b7729508d2923b60d38dd20308
Garv <garvmanchanda2005@gmail.com>
2025-06-28 16:10:14 +0530

    update

--- db.js
+++ db.js
@@ -1,1 +1,32 @@
 const { Schema, default: mongoose } = require("mongoose");
+
+const userSchema = Schema({
+    email: { type: String, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const adminSchema = Schema({
+    email: { type: email, unique: true },
+    password: String,
+    firstName: String,
+    lastName: String
+});
+
+const courseSchema = Schema({
+    title: String,
+    descriptiom: String,
+    price: Number,
+    imageURL: String,
+    creatorID: ObjectID
+});
+
+const purchaseSchema = Schema({
+
+});
+
+const userModel = mongoose.Model("user", userSchema);
+const adminModel = mongoose.Model("admin", adminSchema);
+const courseModel = mongoose.Model("course", courseSchema);
+const purchaseModel = mongoose.Model("purchase", purchaseSchema);
